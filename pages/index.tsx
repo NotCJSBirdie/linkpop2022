e81568866005f5642d085ea14c718e729e470c2a
Carl Serquiña <serquinajhunlo@gmail.com>
2022-03-24 06:17:36 +0800

    version_3

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +13,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header/>
+      <Header />
+
+      <Hero />
     </div>
   );
 };
