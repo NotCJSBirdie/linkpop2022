17abce8e724f3a116257594ba7c4a44e6069a28b
Carl Serquiña <serquinajhunlo@gmail.com>
2022-03-30 22:16:54 +0800

    version_4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Create from "./components/Create";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
       <Header />
 
       <Hero />
+
+      <Create />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-	content: [
-		"./pages/index.tsx",
-		"./pages/_app.tsx",
-		"./pages/components/Header.tsx",
-		"./pages/components/Hero.tsx"
-	],
-	theme: {
-		extend: {}
-	},
-	plugins: []
+  content: [
+    "./pages/index.tsx",
+    "./pages/_app.tsx",
+    "./pages/components/Header.tsx",
+    "./pages/components/Hero.tsx",
+    "./pages/components/Create.tsx",
+    "./pages/components/MarqueeContent.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
